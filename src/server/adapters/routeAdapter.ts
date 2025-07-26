d009cf439e4f590f0a9b27bb957b1c665d6144c7
Pedro Vidal <pedro.vidal@rentbrella.com>
2025-07-25 21:06:16 -0300

    .

--- src/server/adapters/routeAdapter.ts
+++ src/server/adapters/routeAdapter.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from "express";
 import { IController } from "../../application/interfaces/IController";
 
+/**
+ * Adapts an Express request to the controller's handle method and sends the response.
+ *
+ * @param {IController} controller - The controller instance to handle the request.
+ * @returns {Function} A function that takes an Express request and response object.
+ */
+
 export function routeAdapter(controller: IController) {
   return async (req: Request, res: Response) => {
     const { body, statusCode } = await controller.handle({
