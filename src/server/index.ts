ffe6f1effdbc547e07636f931525ce6f55719ac4
Pedro Vidal <pedro.vidal@rentbrella.com>
2025-07-25 21:05:30 -0300

    create route adapter

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,30 +1,14 @@
 import express from "express";
 import { makeSignUpController } from "../factories/makeSignUpContoller";
 import { makeSignInController } from "../factories/makeSignInContoller";
+import { routeAdapter } from "./adapters/routeAdapter";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/sign-up", async (req, res) => {
-  const signUpController = makeSignUpController();
-
-  const { body, statusCode } = await signUpController.handle({
-    body: req.body,
-  });
-
-  res.status(statusCode).json(body);
-});
-
-app.post("/sign-in", async (req, res) => {
-  const singInController = makeSignInController();
-
-  const { body, statusCode } = await singInController.handle({
-    body: req.body,
-  });
-
-  res.status(statusCode).json(body);
-});
+app.post("/sign-in", routeAdapter(makeSignInController()));
+app.post("/sign-up", routeAdapter(makeSignUpController()));
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
